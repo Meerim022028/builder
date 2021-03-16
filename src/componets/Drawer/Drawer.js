70215fa9a7fd1344eb4e85e8ee4faef283a69337
Meerim Toktorova <meritoktorova@mail.com>
2021-03-16 15:34:11 +0600

    added Drawer

---
+++ src/componets/Drawer/Drawer.js
@@ -0,0 +1,10 @@
+import classes from "./Drawer.module.css";
+
+const Drawer =() => {
+    return (
+        <div className={classes.Drawer}>
+           Drawer
+        </div>
+    );
+}
+export default Drawer;
