b99f68f898edbdc0fec532256defad1a30b69d0a
Meerim Toktorova <meritoktorova@mail.com>
2021-03-16 15:38:43 +0600

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-
+import Layout from "./componets/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Layout>
     Pizza Builder by master Meerim
+    </Layout>
     </div>
   );
 }
