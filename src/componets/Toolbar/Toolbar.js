0eb0fadcd389f089c78d65ea44cffd7218df128c
Meerim Toktorova <meritoktorova@mail.com>
2021-03-16 15:39:09 +0600

    Staged Changes

---
+++ src/componets/Toolbar/Toolbar.js
@@ -0,0 +1,10 @@
+import classes from "./Toolbar.module.css";
+
+const Toolbar =() => {
+    return (
+        <div className={classes.Toolbar}>
+           Toolbar
+        </div>
+    );
+}
+export default Toolbar;
