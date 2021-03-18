22731f47f85a61ce6f5f98c94735d719cfb794ff
Meerim Toktorova <meritoktorova@mail.com>
2021-03-18 14:34:07 +0600

    added drower open-closed classes

--- src/componets/Layout/Drawer/Drawer.js
+++ src/componets/Layout/Drawer/Drawer.js
@@ -1,10 +1,16 @@
+import Logo from "../../Logo/Logo";
+import Nav from "../../Nav/Nav";
 import classes from "./Drawer.module.css";
 
 const Drawer = () => {
     return (
-        <div className={classes.Drawer}>
-            Drawer
-        </div>
+     <div className={classes.Drawer}>
+
+         <div className={[classes.content, classes.closed].join(" ")}>
+            <Logo />
+            <Nav /> 
+        </div>  
+    </div>
     );
 }
 
