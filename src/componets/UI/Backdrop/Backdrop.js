1a5200d97a0dadff2fd005a9d7dafb5b62c4cca2
Meerim Toktorova <meritoktorova@mail.com>
2021-03-19 16:02:26 +0600

    Added BackDrop in the drower

---
+++ src/componets/UI/Backdrop/Backdrop.js
@@ -0,0 +1,8 @@
+  
+import classes from "./Backdrop.module.css";
+
+const Backdrop = ({ show, click }) => {
+  return show ? <div className={classes.Backdrop} onClick={click}></div> : null;
+}
+
+export default Backdrop;
