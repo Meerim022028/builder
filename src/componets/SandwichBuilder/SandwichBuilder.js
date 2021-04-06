5cae720b52ee0af625a407cd3deda3468e77e2b9
Meerim Toktorova <meritoktorova@mail.com>
2021-04-06 15:29:41 +0600

    chnages a SandwichBuilder

--- src/componets/SandwichBuilder/SandwichBuilder.js
+++ src/componets/SandwichBuilder/SandwichBuilder.js
@@ -3,28 +3,54 @@ import classes from "./SandwichBuilder.module.css";
 import SandwichPreview from "./SandwichPreview/SandwichPreview";
 import SandwichControls from "./SandwichControls/SandwichControls";
 
+
 const SandwichBuilder = () => {
-  const [ingredients, setIngredients] =  useState({
-    tomato: 15,
-    salami:4,
-    blackOlive: 3,
+  const prices = {
+    tomato: 3.5,
+    salami: 4,
+    blackOlive: .3,
+   
+  };
+  const [ingredients, setIngredients] = useState({
+    tomato: 1,
+    salami: 1,
+    blackOlive: 1,
+    
   });
+  const [price, setPrice] = useState(150);
+  const [canBuy, setCanBuy] = useState(true);
+  // const [isBuying,set]
+  function checkCanBuy(newIngredients) {
+    const totalIngredients = Object.values(newIngredients)
+      .reduce((total, current) => total + current);
+    setCanBuy(totalIngredients > 0);
+  }
+
   function addIngredient(type) {
     const newIngredients = { ...ingredients };
     newIngredients[type]++;
+    checkCanBuy(newIngredients);
+    setPrice(price + prices[type]);
     setIngredients(newIngredients);
   }
 
   function removeIngredient(type) {
-    const newIngredients = { ...ingredients };
-    newIngredients[type]--;
-    setIngredients(newIngredients);
+    if (ingredients[type]) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      checkCanBuy(newIngredients);
+      setPrice(price - prices[type]);
+      setIngredients(newIngredients);
+    }
   }
 
   return (
     <div className={classes.SandwichBuilder}>
-      <SandwichPreview ingredients={ingredients} />
+      <SandwichPreview
+        ingredients={ingredients}
+        price={price} />
       <SandwichControls
+        canBuy={canBuy}
         ingredients={ingredients}
         addIngredient={addIngredient}
         removeIngredient={removeIngredient}
