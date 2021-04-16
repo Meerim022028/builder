bdfd491428264975eb74954af8bdbe0ba6f52a45
Meerim Toktorova <meritoktorova@mail.com>
2021-04-16 16:41:31 +0600

    bv,h

--- src/componets/SandwichBuilder/SandwichIngredient/SandwichIngredient.js
+++ src/componets/SandwichBuilder/SandwichIngredient/SandwichIngredient.js
@@ -1,33 +1,24 @@
 import classes from "./SandwichIngredient.module.css";
-
-import salamibackground from "../../../images/salami.svg";
-//  import tomatoBackground from "../../../images/tomato.svg";
-import blackOliveBackground from "../../../images/blackOlive.svg";
-import cucumberBackground from "../../../images/cucumber.svg";
+// import ketchupbackground from "../../../images/ketchup.svg";
+import tomatoBackground from "../../../images/tomato.svg";
+import baconBackground from "../../../images/bacon.svg";
+import onionsBackground from "../../../images/onions.svg";
 import cheeseBackground from "../../../images/cheese.svg";
-import meatBackground from "../../../images/meat.svg"
-import saladBackground from "../../../images/salad.svg"
+import meatBackground from "../../../images/meat.svg";
+import saladBackground from "../../../images/salad.svg";
 
 
 
 
 import React from "react";
-
 const SandwichIngredient = ({ type }) => {
   const types = {
-    // salami: { backgroundImage: `url(${salamiBackground})`, width: "35px", height: "35px" },
-    salami: {backgroundColor: "black", width: "100%", height: "30px"},
-    salami: {backgroundImage: `url(${salamibackground})`, width: "30px", height: "30px"},
-    tomato: { backgroundColor: "tomato", width: "100%", height: "35px" },
-    blackOlive: { backgroundImage: `url(${blackOliveBackground})`, width: "10px", height: "10px" },
-    cucumber: { backgroundImage: `url(${cucumberBackground})`, width: "30px", height: "30px" },
-    cheese: { backgroundImage: `url(${cheeseBackground})`, width: "35px", height: "35px" },
-    meat: { backgroundImage: `url(${meatBackground})`, width: "45px", height: "45px" },
-    salad: { backgroundImage: `url(${saladBackground})`, width: "45px", height: "45px" },
-
-
-
-
+    tomato: { backgroundImage: `url(${tomatoBackground})`, width: "100%", height: "30px" },
+    bacon: { backgroundImage: `url(${baconBackground})`, width: "100%", height: "10px" },
+    onions: { backgroundImage: `url(${onionsBackground})`, width: "100%", height: "30px" },
+    cheese: { backgroundImage: `url(${cheeseBackground})`, width: "100%", height: "60px" },
+    meat: { backgroundImage: `url(${meatBackground})`, width: "100%", height: "45px" },
+    salad: { backgroundImage: `url(${saladBackground})`, width: "100%", height: "45px" },
   };
 
 
@@ -53,9 +44,9 @@ const SandwichIngredient = ({ type }) => {
   }
 
   // Get random position for this ingredient.
-  const position = getPosition(types[type].width);
-  types[type].top = position.top + "px";
-  types[type].left = position.left + "px";
+  // const position = getPosition(types[type].width);
+  // types[type].top = position.top + "px";
+  // types[type].left = position.left + "px";
   // Get random rotation for this ingredient.
   // types[type].transform = `rotate(${Math.round(Math.random() * 10)}deg)`;
 
