c0dd5ca51933ca563bf848a1afaca2611a85083a
Meerim Toktorova <meritoktorova@mail.com>
2021-04-16 16:38:30 +0600

    changes cvb

---
+++ src/componets/Checkout/Checout.js
@@ -0,0 +1,15 @@
+import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+
+const Checkout = ({ history }) => {
+  function cancelCallback() {
+    history.replace('/');
+  }
+
+  return (
+    <div>
+      <CheckoutSummary cancelCallback={cancelCallback} />
+    </div>
+  );
+}
+ 
+export default Checkout;
