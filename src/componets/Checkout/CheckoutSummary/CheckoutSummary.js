8c04cad50a2393cbfa5205c83f1ab6e37e70e769
Meerim Toktorova <meritoktorova@mail.com>
2021-04-16 16:39:09 +0600

    added new components

---
+++ src/componets/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,22 @@
+import classes from "./CheckoutSummary.module.css";
+import SandwichPreview from "../../SandwichBuilder/SandwichPreview/SandwichPreview";
+import Button from "../../UI/Button/Button";
+
+const CheckoutSummary = ({ cancelCallback }) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <div>
+        <SandwichPreview ingredients={{
+          tomato: 5, 
+          salami: 10,
+        }} price={150} />
+      </div>
+      <div>
+        <Button>Checkout</Button>
+        <Button onClick={cancelCallback}>Cancel</Button>
+      </div>
+    </div>
+  );
+}
+ 
+export default CheckoutSummary;
