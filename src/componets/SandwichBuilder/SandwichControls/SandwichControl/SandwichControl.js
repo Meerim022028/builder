65215523f81b1e9ad8d8ae6634a3b457d41c6da8
Meerim Toktorova <meritoktorova@mail.com>
2021-04-22 22:19:10 +0600

    ertyu

--- src/componets/SandwichBuilder/SandwichControls/SandwichControl/SandwichControl.js
+++ src/componets/SandwichBuilder/SandwichControls/SandwichControl/SandwichControl.js
@@ -1,17 +1,28 @@
-import SandwichIngredient from "../../SandwichIngredient/SandwichIngredient";
+
 import classes from "./SandwichControl.module.css";
 import Button from "../../../UI/Button/Button";
 
+
 const SandwichControl = ({ type, add, remove }) => {
-    return (
-      <div className={classes.SandwichControl}>
-        <Button  onClick={() => add(type)}>+</Button>
-        <div className={classes.ingredient}>
-          <SandwichIngredient type={type} fixed />
-        </div>
-        <Button onClick={() => remove(type)}>-</Button>
-      </div>
-    );
+  const name ={
+    cheese:"cheese",
+    tomato:" tomato",
+    salad:"salad",
+    meat:"meat",
+    bacon:"bacon",
+    onion:"onion",
   }
+  return (
+    <div className={classes.SandwichControl}>
+ <Button onClick={() => add(type)}>+</Button>
+      <div className={classes.ingredient}>
+        {/* <ToysIngredient type={type} fixed /> */}
+        {name[type]}
+      </div>
+      <Button onClick={() => remove(type)}>-</Button>
+    </div>
+  );
+}
+
   
 export default SandwichControl;
