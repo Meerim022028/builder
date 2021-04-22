e7d62e47aa1a379057354e3bf507674d19eb2d93
Meerim Toktorova <meritoktorova@mail.com>
2021-04-22 22:19:53 +0600

    changes tolbar

--- src/componets/Layout/Toolbar/Toolbar.js
+++ src/componets/Layout/Toolbar/Toolbar.js
@@ -1,18 +1,38 @@
-import classes from "./Toolbar.module.css";
-import Logo from "../../UI/Logo/Logo";
-import Nav from "../../Nav/Nav"
+// import classes from "./Toolbar.module.css";
+// import Logo from "../../UI/Logo/Logo";
+// import Nav from "../../Nav/Nav"
+// import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
+
+
+
+// const Toolbar = ({ openDrawer }) => {
+//     return (
+//       <div className={classes.Toolbar}>
+//         <Logo />
+//         <Nav />
+//         <DrawerOpen click={openDrawer} />
+//       </div>
+//     );
+//   }
+   
+//   export default Toolbar;
+
 import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
+import Logo from "../../UI/Logo/Logo";
+import Nav from "../../Nav/Nav";
 
-
+import classes from "./Toolbar.module.css";
 
 const Toolbar = ({ openDrawer }) => {
-    return (
-      <div className={classes.Toolbar}>
-        <Logo />
+  return (
+    <div className={classes.Toolbar}>
+      <Logo />
+      <nav>
         <Nav />
-        <DrawerOpen click={openDrawer} />
-      </div>
-    );
-  }
-   
-  export default Toolbar;
+      </nav>
+      <DrawerOpen click={openDrawer} />
+    </div>
+  );
+}
+
+export default Toolbar;
