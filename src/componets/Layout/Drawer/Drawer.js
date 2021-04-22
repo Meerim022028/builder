058ca494f732f85fcefa32873bd18ad1b2b9d1de
Meerim Toktorova <meritoktorova@mail.com>
2021-04-22 22:20:22 +0600

    chnage

--- src/componets/Layout/Drawer/Drawer.js
+++ src/componets/Layout/Drawer/Drawer.js
@@ -1,12 +1,36 @@
+// import Logo from "../../UI/Logo/Logo";
+// import Nav from "../../Nav/Nav";
+// import Backdrop from "../../UI/Backdrop/Backdrop";
+// import classes from "./Drawer.module.css";
+
+// const Drawer = ({ open, closeDrawer }) => {
+//   const drawerClasses = [
+//     classes.content,
+//     open ? classes.open : classes.closed
+//   ];
+
+//   return (
+//     <div className={classes.Drawer}>
+//       <Backdrop show={open} click={closeDrawer} />
+//       <div className={drawerClasses.join(" ")}>
+//         <Logo />
+//         <Nav />
+//       </div>
+//     </div>
+//   );
+// }
+ 
+// export default Drawer;
 import Logo from "../../UI/Logo/Logo";
 import Nav from "../../Nav/Nav";
 import Backdrop from "../../UI/Backdrop/Backdrop";
+
 import classes from "./Drawer.module.css";
 
 const Drawer = ({ open, closeDrawer }) => {
   const drawerClasses = [
     classes.content,
-    open ? classes.open : classes.closed
+    open ? classes.open : classes.close
   ];
 
   return (
@@ -19,5 +43,5 @@ const Drawer = ({ open, closeDrawer }) => {
     </div>
   );
 }
- 
+
 export default Drawer;
