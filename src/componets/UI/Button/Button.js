d50ce2582fe4675aab8cb099a4a6acaf00e69c57
Meerim Toktorova <meritoktorova@mail.com>
2021-04-30 14:21:30 +0600

    sadfghj

--- src/componets/UI/Button/Button.js
+++ src/componets/UI/Button/Button.js
@@ -1,8 +1,13 @@
 import classes from "./Button.module.css";
 
 const Button = (props) => {
+  const className = [classes.Button];
+  if (props.green) {
+    className.push(classes.green);
+  }
+
   return (
-    <button {...props} className={classes.Button}>{props.children}</button>
+    <button {...props} className={className.join(' ')}>{props.children}</button>
   );
 }
 
