f73dc77ab0eef11dcb7b7d7c6842292dee11b41d
Meerim Toktorova <meritoktorova@mail.com>
2021-04-30 14:22:19 +0600

    dfg

--- src/componets/Checkout/Checout.js
+++ src/componets/Checkout/Checout.js
@@ -1,15 +1,44 @@
 import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+import axios from "axios";
+
 
 const Checkout = ({ history }) => {
   function cancelCallback() {
     history.replace('/');
   }
 
+  function submitCallback(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    const order = {
+      name: data.get('name'),
+      phone: data.get('phone'),
+      address: data.get('address'),
+      ingredients: {
+        cheese:2,
+        tomato: 3.5,
+        meat:4,
+        salad:2,
+        Bacon:2,
+        onions:1,
+      }
+    }
+
+    axios.post('https://builder2-f8ec3-default-rtdb.firebaseio.com/orders.json', order)
+      .then(response => {
+        history.replace('/');
+      });
+  }
+
   return (
     <div>
-      <CheckoutSummary cancelCallback={cancelCallback} />
+      <CheckoutSummary
+        submitCallback={submitCallback}
+        cancelCallback={cancelCallback} />
     </div>
   );
 }
+ 
  
 export default Checkout;
