b677f3c1ce5a180730fd73dcc2f126a0fa0366b1
Meerim Toktorova <meritoktorova@mail.com>
2021-05-04 15:32:36 +0600

    changes

--- src/componets/Checkout/Checout.js
+++ src/componets/Checkout/Checout.js
@@ -1,44 +1,42 @@
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+import SandwichPreview from "../SandwichBuilder/SandwichPreview/SandwichPreview";
+import CheckoutForm from "../Checkout/CheckoutSummary/CheckoutForm/CheckoutForm";
+import classes from "./Checout.module.css";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 
 const Checkout = ({ history }) => {
+  const ingredients = useSelector(state => state.ingredients);
+  const price = useSelector(state => state.price);
+
   function cancelCallback() {
     history.replace('/');
   }
 
   function submitCallback(event) {
-    event.preventDefault();
-
     const data = new FormData(event.target);
-    const order = {
-      name: data.get('name'),
-      phone: data.get('phone'),
-      address: data.get('address'),
-      ingredients: {
-        cheese:2,
-        tomato: 3.5,
-        meat:4,
-        salad:2,
-        Bacon:2,
-        onions:1,
-      }
-    }
 
-    axios.post('https://builder2-f8ec3-default-rtdb.firebaseio.com/orders.json', order)
-      .then(response => {
-        history.replace('/');
-      });
+    axios.post('https://builder2-f8ec3-default-rtdb.firebaseio.com/orders.json', {
+      name: data.get('name'),
+      address: data.get('address'),
+      phone: data.get('phone'),
+      ingredients: ingredients,
+      price: price,
+    }).then(response => {
+      history.replace('/');
+    });
+
+    event.preventDefault();
   }
 
   return (
-    <div>
-      <CheckoutSummary
-        submitCallback={submitCallback}
-        cancelCallback={cancelCallback} />
+    <div className={classes.Checkout}>
+      <SandwichPreview ingredients={ingredients} price={price} />
+      <CheckoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback} />
     </div>
   );
 }
- 
  
 export default Checkout;
