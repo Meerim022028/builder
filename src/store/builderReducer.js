26fecd7286e0b2d90cd09b012e9c1d15bff21d96
Meerim Toktorova <meritoktorova@mail.com>
2021-05-04 14:51:48 +0600

    changes builderReducer

--- src/store/builderReducer.js
+++ src/store/builderReducer.js
@@ -1,7 +1,39 @@
-const initialState ={};
+const initialState ={
+    ingredients: {
+        cheese:2,
+        tomato: 2,
+        meat:2,
+        salad:2,
+        Bacon:2,
+        onions:1,
+    },
+    price:150,
+};
+const prices = {
+    cheese:2,
+    tomato: 3.5,
+    meat:4,
+    salad:2,
+    Bacon:2,
+    onions:1,
+  };
 
 const builderReducer = (state = initialState, action) => {
-    return action;
+    const newState = { ...state};
+
+    switch (action.type) {
+        case "ADD_INGREDIENT":
+            newState.ingredients[action.ingredient]++;
+            newState.price += prices[action.ingredient];
+            break;
+    case "REMOVE_INGREDIENT":
+        newState.ingredients[action.ingredient]--;
+        break;
+
+        default:
+            break;
+    }
+    return newState;
 }
 
 export default builderReducer;
