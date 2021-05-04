ae8e3bc04c8cf280267e12b60e67780ce5af6458
Meerim Toktorova <meritoktorova@mail.com>
2021-05-04 14:37:45 +0600

    changes

--- src/componets/SandwichBuilder/SandwichControls/SandwichControl/SandwichControl.js
+++ src/componets/SandwichBuilder/SandwichControls/SandwichControl/SandwichControl.js
@@ -1,9 +1,11 @@
 
 import classes from "./SandwichControl.module.css";
 import Button from "../../../UI/Button/Button";
+import { useDispatch } from "react-redux";
 
 
-const SandwichControl = ({ type, add, remove }) => {
+const SandwichControl = ({ type, count }) => {
+  const dispatch = useDispatch();
   const name ={
     cheese:"cheese",
     tomato:" tomato",
@@ -14,12 +16,12 @@ const SandwichControl = ({ type, add, remove }) => {
   }
   return (
     <div className={classes.SandwichControl}>
- <Button onClick={() => add(type)}>+</Button>
+ <Button onClick={() => dispatch({ type: "ADD_INGREDIENT", ingredient:type })}>+</Button>
       <div className={classes.ingredient}>
-        {/* <ToysIngredient type={type} fixed /> */}
+       
         {name[type]}
       </div>
-      <Button onClick={() => remove(type)}>-</Button>
+      <Button onClick={() => dispatch({ type: "REMOVE_INGREDIENT", ingredient:type })} disabled={!count}>-</Button>
     </div>
   );
 }
