fd1e35f79265efad042261dac5d4efc35cded9f1
Meerim Toktorova <meritoktorova@mail.com>
2021-05-12 16:00:39 +0600

    asd

---
+++ src/componets/Orders/Orders.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { load } from "../../store/actions/orders";
+import Order from "./Order/Order";
+import withAxios from "../withAxios";
+import axios from "../../axios";
+import classes from "./Orders.module.css";
+
+const Orders = () => {
+  const dispatch = useDispatch();
+  const orders = useSelector(state => state.orders);
+
+  useEffect(() => dispatch(load()), [dispatch]);
+
+  const results = orders.map(order => <Order key={order.id} {...order} />);
+
+  return (
+    <div className={classes.Orders}>
+      {results}
+    </div>
+  );
+}
+
+export default withAxios(Orders, axios);
