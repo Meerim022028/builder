fecfc63113e6d5dd710a027ebc14b74695682ccd
Meerim Toktorova <meritoktorova@mail.com>
2021-05-18 14:07:09 +0600

    changes axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create();
-instance.defaults.baseURL = "https://builder-9f6b5-default-rtdb.firebaseio.com";
+instance.defaults.baseURL = "https://builder2-f8ec3-default-rtdb.firebaseio.com";
 
 export default instance;
