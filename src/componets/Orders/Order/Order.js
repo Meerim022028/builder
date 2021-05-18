fe480973ddf3ccda40de4a2f0423bda82807643a
Meerim Toktorova <meritoktorova@mail.com>
2021-05-18 15:47:38 +0600

    rtt

--- src/componets/Orders/Order/Order.js
+++ src/componets/Orders/Order/Order.js
@@ -1,16 +1,17 @@
 import classes from "./Order.module.css";
 
-const Order = ({ name, phone, address, ingredients, price }) => {
-  const outputIngredients = Object.keys(ingredients)
-    .map(ingredient => <em key={ingredient}>{ingredient} - {ingredients[ingredient]}</em>);
-
+const Order = ({ name, phone, address, ingredients,price }) => {
+  const output = Object.keys(ingredients).map(label => <em key={label}>{label} - {ingredients[label]}</em>)
   return (
-    <div className={classes.Order}>
-      <div>{name}, {phone}, {address}</div>
-      <div>{outputIngredients}</div>
-      <strong>{price}</strong>
-    </div>
+    <ul className={classes.Order}>
+      <li>Name: {name}</li>
+      <li>Phone: {phone}</li>
+      <li>Address: {address}</li>
+      <li>Ingredients: {output}</li>
+     <strong>Price:{price}</strong>  
+       </ul>
   );
 }
 
 export default Order;
+
