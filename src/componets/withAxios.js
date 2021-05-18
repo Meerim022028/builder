4fdb425441eb9b1b96b77beb062ffc1c2a706b86
Meerim Toktorova <meritoktorova@mail.com>
2021-05-18 14:07:20 +0600

    added without

--- src/componets/withAxios.js
+++ src/componets/withAxios.js
@@ -1,13 +1,44 @@
-const reportWebVitals = onPerfEntry => {
-    if (onPerfEntry && onPerfEntry instanceof Function) {
-      import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
-      });
+import React, { useState, useEffect } from "react";
+import Modal from "./UI/Modal/Modal";
+
+const withAxios = (WrappedComponent, axios) => {
+  return (props) => {
+    const [error, setError] = useState(null);
+
+    function hideModal() {
+      setError(false);
     }
+
+    const requestInterceptor = axios.interceptors.request.use((request) => {
+      setError(false);
+      return request;
+    });
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        setError(error);
+        return Promise.reject(error);
+      }
+    );
+
+    useEffect(() => {
+      return () => {
+        axios.interceptors.request.eject(requestInterceptor);
+        axios.interceptors.response.eject(responseInterceptor);
+      };
+    }, [requestInterceptor, responseInterceptor]);
+
+    return (
+      <>
+        <Modal show={error} hideCallback={hideModal}>
+          {error ? error.message : "Unknown error"}
+        </Modal>
+        <WrappedComponent {...props} />
+      </>
+    );
   };
-  
-  export default reportWebVitals;
+};
+
+export default withAxios;
