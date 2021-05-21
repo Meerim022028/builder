96dd8595aff07278b76b0e83a9bc26c803dbd2af
Meerim Toktorova <meritoktorova@mail.com>
2021-05-21 16:58:09 +0600

    changes app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Layout from "./componets/Layout/Layout";
 import SandwichBuilder from "./componets/SandwichBuilder/SandwichBuilder";
 import Checkout from "./componets/Checkout/Checout";
 import Orders from "./componets/Orders/Orders";
+import strong from "../src/images/logo2.png";
 
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router";
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <Layout>
+      <img className="strong" src={strong} alt="#" />
         <Switch>
           <Route path="/" component={ SandwichBuilder} exact />
           <Route path="/checkout" component={Checkout} />
